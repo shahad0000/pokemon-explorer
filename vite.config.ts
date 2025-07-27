4627148e7b0d58b8d6c6b3c6082dded7b908c79a
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-07-28 05:14:36 +0600

    add host and port to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		host: '0.0.0.0',
+		port: process.env.PORT ? Number(process.env.PORT) : 5173
+	},
 	plugins: [tailwindcss(), sveltekit()]
 });
