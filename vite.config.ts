c0015885b80796ebcef75e27cb04a7cc06664964
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-07-28 05:18:57 +0600

    allow host in  vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	server: {
-		host: '0.0.0.0',
-		port: process.env.PORT ? Number(process.env.PORT) : 5173
+		allowedHosts: ['pokemon-explorer-ykv5.onrender.com']
 	},
 	plugins: [tailwindcss(), sveltekit()]
 });
