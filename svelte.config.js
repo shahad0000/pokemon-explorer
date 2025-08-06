1efee728dcf0c81ed9b8e3bfb83aec5ba84c5820
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-08-07 01:47:06 +0300

    code optimization

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	},
 	vitePlugin: {
 		runes: true 
